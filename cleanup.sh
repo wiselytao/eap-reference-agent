cb110ab7d12ade4a0e5bf6c0d43e0d2b0a1abe0b
Wisely Tao <wisely.tao@gmail.com>
2026-01-09 02:39:14 +0800

    Add restart.sh and update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,5 @@ find . -type d -name "__pycache__" -prune -exec rm -rf {} +
 find . -type d -name ".*" -prune -exec rm -rf {} +
 rm -fr PRD
 rm -fr Reference
+rm -f AGENTS.md
+rm -fr data/*
---
+++ scripts/restart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+scripts/stop.sh
+scripts/start.sh
